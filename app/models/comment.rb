a22b28fbd049b9051cc396664d53baf7c416c148
stickler-ci <support@stickler-ci.com>
2019-05-09 13:31:38 +0000

    Fixing style errors.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class Comment < ApplicationRecord
   validates :comment, presence: true
   validates :user_id, presence: true
   validates :post_id, presence: true
-  
+
   belongs_to :user
   belongs_to :post
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  validates :name, presence: true, length: {maximum: 25}
-  validates :email, presence: true, length: {maximum: 255}
-  validates :password, presence: true, length: {minimum: 6}
+  validates :name, presence: true, length: { maximum: 25 }
+  validates :email, presence: true, length: { maximum: 255 }
+  validates :password, presence: true, length: { minimum: 6 }
 
   has_many :posts
   has_many :comments
