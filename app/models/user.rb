8565fc6db31b84b3bd4cadba408db793087d2203
bellom <bellomsean@gmail.com>
2019-05-09 12:31:08 +0100

    added association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 25}
   validates :email, presence: true, length: {maximum: 255}
   validates :password, presence: true, length: {minimum: 6}
+
+  has_many :posts
 end
